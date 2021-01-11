8364c0456f4bc42331d5280c7ae317857f03a11d
chidraeve <lukas.mainka@clubitsolutions.de>
2021-01-11 13:03:39 +0100

    87. Search Logs

--- src/components/layout/Searchbar.js
+++ src/components/layout/Searchbar.js
@@ -1,16 +1,29 @@
-import React from "react"
+import React, { useRef } from "react"
+import { connect } from "react-redux"
+import { searchLogs } from "../../actions/logActions"
+import PropTypes from "prop-types"
 
-const Searchbar = () => {
+const Searchbar = ({ searchLogs }) => {
+	const text = useRef("")
+
+	const onChange = e => searchLogs(text.current.value)
+
 	return (
-		<nav style={{ marginBottom: '30px' }} className='blue'>
-			<div className='nav-wrapper'>
+		<nav style={{ marginBottom: "30px" }} className="blue">
+			<div className="nav-wrapper">
 				<form>
-					<div className='input-field'>
-						<input id='search' type='search' />
-						<label className='label-icon' htmlFor='search'>
-							<i className='material-icons'>search</i>
+					<div className="input-field">
+						<input
+							id="search"
+							type="search"
+							placeholder="Search Logs..."
+							ref={text}
+							onChange={onChange}
+						/>
+						<label className="label-icon" htmlFor="search">
+							<i className="material-icons">search</i>
 						</label>
-						<i className='material-icons'>close</i>
+						<i className="material-icons">close</i>
 					</div>
 				</form>
 			</div>
@@ -18,4 +31,8 @@ const Searchbar = () => {
 	)
 }
 
-export default Searchbar
+Searchbar.prototype = {
+	searchLogs: PropTypes.func.isRequired
+}
+
+export default connect(null, { searchLogs })(Searchbar)
